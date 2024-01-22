e6889d2138fee5336abfdca0a643335dd680e8a4
Shyon Ghahghahi <sghahghahi2@gojira.cs.usfca.edu>
2024-01-22 15:24:30 -0800

    Changing name of operating system

--- user/about.c
+++ user/about.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-	printf("Welcome to SOS!\n");
+	printf("Welcome to tacOS!\n");
 	printf("--------------------------\n");
 	printf("Author: Shyon Ghahghahi\n");
 	printf("USF Username: sghahghahi2\n");
