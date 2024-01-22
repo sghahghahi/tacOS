081ab402ddc8a1799407535aba3b8927207e994b
Shyon Ghahghahi <sghahghahi2@gojira.cs.usfca.edu>
2024-01-22 12:39:19 -0800

    Lab 0

---
+++ user/about.c
@@ -0,0 +1,13 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(void)
+{
+	printf("Welcome to SOS!\n");
+	printf("--------------------------\n");
+	printf("Author: Shyon Ghahghahi\n");
+	printf("USF Username: sghahghahi2\n");
+	
+	return 0;
+}
